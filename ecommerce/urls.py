dfa716bfadcf8b7d8a86869ddf71599ef1472cb0
Chris Zielinski <ckz8780@gmail.com>
2018-06-24 19:32:20 +0000

    Added search app and base site styling

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from accounts import urls as urls_accounts
 from products import urls as urls_products
 from cart import urls as urls_cart
+from search import urls as urls_search
 from products.views import all_products
 from django.conf import settings
 from django.conf.urls.static import static
@@ -28,4 +29,5 @@ urlpatterns = [
     path('accounts/', include(urls_accounts)),
     path('products/', include(urls_products)),
     path('cart/', include(urls_cart)),
+    path('search/', include(urls_search)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
