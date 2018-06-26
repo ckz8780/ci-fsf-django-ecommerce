d31b7be1baa9472e215b418ee089775e2625d343
Chris Zielinski <ckz8780@gmail.com>
2018-06-26 02:53:26 +0000

    Updated media location to S3 bucket

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = (
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
